34c1e808afc96445566a7599054e857a3cf6aae5
Joshua Biong <joshuabiong7@gmail.com>
2024-03-21 22:25:08 +0800

    create the hero section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "01272eced48e949b"; }
+/******/ 		__webpack_require__.h = function() { return "98980fc1e74e307e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,33 @@
+import Link from "next/link"
+import Image from "next/image"
+import hero from "../../public/assets/hero.png"
+
 export default function HomePage() {
-    return <div>Test</div>
+    return <>
+
+    <div className="grid-halves h-screen-navbar">
+        <div className="bg-teal boarder-right">
+            <div className="column-padding">
+                <div className="tablet-centered">
+                    <div className="content-grid home-hero">
+                        <h1>Great HTML <br/>Website Template</h1>
+                        <p className="section-subtitle">Create your own Website in no time!</p>
+                        <Link href="/products" className="large-button">
+                            <div className="large-button-text">Expore Products</div>
+                        </Link>
+                    </div>               
+                </div>
+            </div>
+        </div>
+        <div className="bg-salmon">
+            <div className="collumn-padding centered">
+                <div className="callout-wrap">
+                    <Image src={hero} placeholder="blur" className="callout-image"></Image>
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
 }
+    
+
